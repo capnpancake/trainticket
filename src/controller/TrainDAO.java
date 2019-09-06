0eb0c59d88719928ba832e8a66f3dfffa5c29b55
Andrew <andrewspatrick13@gmail.com>
2019-09-06 15:35:32 -0400

    setting up dao

--- src/controller/TrainDAO.java
+++ src/controller/TrainDAO.java
@@ -1,5 +1,18 @@
 package controller;
 
+import java.sql.*;
+
 public class TrainDAO {
     Connection con;
+    Statement stmt;
+
+    public TrainDAO() throws SQLException, ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "hr");
+        stmt = con.createStatement();
+    }
+    
+    public Train select(int id){
+        return new Train();
+    }
 }
