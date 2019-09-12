3d9c246d6e9bf9fe6c51f42e087e0588cf0b7d97
Andrew <andrewspatrick13@gmail.com>
2019-09-12 12:13:15 -0400

    rough draft

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -1,44 +1,107 @@
 package model;
 
-import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Ticket {
 
-    private static int counter;
+    private static int counter = 100;
     private String pnr;
     private Date travelDate;
     private Train train;
-    private TreeMap<Passenger, Integer> passengers;
+    private TreeMap<Passenger, Double> passengers;
 
     public Ticket(Date travelDate, Train train) {
         this.travelDate = travelDate;
         this.train = train;
+        pnr = generatePNR();
+        counter += 1;
     }
 
     private String generatePNR(){
-    //
-
+        // return a string formatted as SD_YYYYMMDD_100
+        // where S = train.source, D = train.destination, and 100 is the counter
+        DateFormat df = new SimpleDateFormat("yyyymmdd");
+        return train.getSource().charAt(0) +
+                train.getDestination().charAt(0) + "_" +
+                df.format(travelDate) + "_" +
+                counter;
     }
 
 
-    private double calcPassengerFare(Passenger passengers){
+    private double calcPassengerFare(Passenger passenger){
+        // calculate and return fare for passenger
+        double fare = getTrain().getTicketPrice();
 
+        // determine discount based on age
+        if (passenger.getAge() <= 12){
+            fare *= 0.5;
+        } else if (passenger.getAge() >= 60){
+            fare *= 0.6;
+        }
+        // determine discount for gender
+        if (passenger.getGender() == 'F'){
+            fare *= 0.75;
+        }
+
+        return fare;
     }
 
-    public void addPassenger(String string, int num, char character){
-
+    public void addPassenger(String name, int age, char gender){
+        Passenger pssngr = new Passenger(name, age, Character.toUpperCase(gender));
+        passengers.put(pssngr, calcPassengerFare(pssngr));
     }
 
     private double calculateTotalTicketPrice(){
-
+        double total = 0;
+        for (Map.Entry<Passenger, Double> entry : passengers.entrySet()){
+            total += entry.getValue();
+        }
+        return total;
     }
 
     private StringBuilder generateTicket(){
+        // set up template and fill in upper portion
+        StringBuilder td = new StringBuilder()
+                .append("PNR          : ").append(pnr).append("\n")
+                .append("Tran No.     : ").append(train.getTrainNo()).append("\n")
+                .append("Train Name   : ").append(train.getTrainName()).append("\n")
+                .append("From         : ").append(train.getSource()).append("\n")
+                .append("To           : ").append(train.getDestination()).append("\n")
+                .append("Travel Date  : ").append(travelDate).append("\n\n")
+                .append("Passengers:" + "\n")
+                .append("Name\t\tAge\tGender\tFare");
 
+        // use a foreach to iterate through the treemap and print all passengers
+        for (Map.Entry<Passenger, Double> entry : passengers.entrySet()){
+            String name = entry.getKey().getName();
+            int age = entry.getKey().getAge();
+            char gender = entry.getKey().getGender();
+            td.append(name).append("\t").append(age).append("\t").append(gender).append("\t").append(entry.getValue());
+        }
+
+        // finish by showing total price
+        td.append("Total Price : " + "\t\t\t").append(calculateTotalTicketPrice());
+
+        // return fleshed out stringbuilder
+        return td;
     }
 
-    public void writeTicket(){
-
+    public void writeTicket() throws IOException {
+        String file = "E:\\tickets\\" + getPnr() + ".txt";
+        Path path = Paths.get(file);
+        if (!Files.exists(path)){ Files.createFile(path); }
+        FileWriter fw = new FileWriter(file);
+        fw.write(generateTicket().toString());
+        fw.close();
     }
 
     public static int getCounter() {
@@ -73,22 +136,13 @@ public class Ticket {
         this.train = train;
     }
 
-    public TreeMap<Passenger, Integer> getPassengers() {
+    public TreeMap<Passenger, Double> getPassengers() {
         return passengers;
     }
 
-    public void setPassengers(TreeMap<Passenger, Integer> passengers) {
+    public void setPassengers(TreeMap<Passenger, Double> passengers) {
         this.passengers = passengers;
     }
-
-
-
-
-    public static void main(String[] args) {
-
-
-    }
-
 }
 
 
--- src/view/TicketApplication.java
+++ src/view/TicketApplication.java
@@ -1,4 +1,49 @@
 package view;
 
+import dataAccessLayer.TrainDAO;
+import model.Ticket;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class TicketApplication {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TrainDAO dao = new TrainDAO();
+
+        System.out.println("Enter the date (dd/MM/yyyy) you will be traveling on : ");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date date =  new Date();
+        try {
+            date = sdf.parse(scan.nextLine());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        dao.displayTrainSchedule();
+        System.out.print("Please choose a train to purchase tickets for: ");
+        Ticket ticket = new Ticket(date, dao.findTrain(Integer.parseInt(scan.nextLine())));
+
+        System.out.print("How many passengers are you purchasing tickets for? ");
+        int passengers = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < passengers; i++){
+            System.out.print("Enter Passenger Name : ");
+            String name = scan.nextLine();
+            System.out.print("Passenger's Age : ");
+            int age = Integer.parseInt(scan.nextLine());
+            System.out.print("Passenger's Gender (M/F) : ");
+            char gender = scan.nextLine().charAt(0);
+            ticket.addPassenger(name, age, gender);
+        }
+
+        try {
+            ticket.writeTicket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ticket booked with PNR: " + ticket.getPnr());
+    }
 }
